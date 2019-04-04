a5059760bdade86f1b4a5e2051f92c174c6104a5
s1mran <35377484+s1mran@users.noreply.github.com>
2019-04-05 03:05:44 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,11 @@
+# importing requests package 
+import requests
+# url of top 
+url = " https://newsapi.org/v2/top-headlines?country=in&sort_by=top&category=business&apiKey=f6e1c6b98075456692613b67879c33f5"
+
+	# fetching data in json format 
+data = requests.get(url).json() 
+
+	# getting all articles in a string article 
+#print data["totalResults"]
+article = data["articles"] 
