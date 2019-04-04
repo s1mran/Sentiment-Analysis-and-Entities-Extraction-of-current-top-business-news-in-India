e969b7df28a6d8b9ac58bfbb2a31e6cc721b12f5
s1mran <35377484+s1mran@users.noreply.github.com>
2019-04-05 03:23:42 +0530

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('vader_lexicon')
+
+#importing SentimentIntensityAnalyzer package for vader sentiment analysis
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
+
+def result(sentence):
+    score = analyser.polarity_scores(sentence)
+    print("{:-<22} {}".format("Snetiment Result", str(score)))
+    print ("Sentiment Score: " + str(score['compound']) )	
+    
+for i in range(10): 
+		# printing sentiment scores of each news:
+        print(str(i+1)+" "+titles[i])
+        # sentiment scores for this news is:
+        result(descriptions[i])
+        
+        
+        
+	
+				 
